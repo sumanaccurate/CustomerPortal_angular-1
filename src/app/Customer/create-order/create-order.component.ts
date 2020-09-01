0ca197bf423adc79a521d1fa38b54bc66878883b
ahmedshaikh123 <ahmedshaikh78699@gmail.com>
2020-09-01 17:14:23 +0530

    Added DDL in Create Customer and Get address from DDL

--- src/app/Customer/create-order/create-order.component.ts
+++ src/app/Customer/create-order/create-order.component.ts
@@ -10,32 +10,49 @@ import { CustomerService } from 'src/app/shared/CustomerService';
 })
 export class CustomerCreateOrderComponent implements OnInit {
   CustomerData: any; 
+  ShipToName;
+  ItemMaster;
   constructor(private _CustomerService: CustomerService, private router: Router, @Inject(SESSION_STORAGE) private storage: WebStorageService) { }
- 
+  ShiptoAddresss: any; 
   projects={projectID: 'wxp001', projectName: 'TYC001', dateOfStart: '2018-12-23', teamSize: 'L', inedit: false};
   ngOnInit() {
     let Userid= localStorage.getItem('UserCode');
     this.getCustomerData(Userid);
+    this.getShiptoAddressData(Userid);
+    this.GetTopItemMaster();
   }
 
-  onEditClick(event, index: number) {
-    // modify the vlaue of inedit
-    this.projects[index].inedit = true;
+  getShipToNameData(Userid){
+    if(Userid!==null &&Userid!==""){
+      this._CustomerService.GetShipToAddress(Userid).subscribe((res: any) => {  
+        this.ShipToName = res['0'].Addressvtxt;
+      })  
+    }
+    else{
+      this.router.navigate(['/user/login']);
+    }
+  }
+
   
-    // code logic here
-    // ...
-    
-    
+  getShiptoAddressData(Userid){
+    if(Userid!==null &&Userid!==""){
+     
+      this._CustomerService.getGetShipToData(Userid).subscribe(  
+        data => {  
+         this.ShiptoAddresss = data;
+        }  
+      );  
+    }
   }
 
- 
-  onCancelClick(index: number) {
-    this.projects[index].inedit = false;
   
-    // code logic here
-    // ...
+  GetTopItemMaster(){
+      this._CustomerService.GetTopItemMaster().subscribe(  
+        data => {  
+         this.ItemMaster = data;
+        }  
+      );  
   }
-
 
   getCustomerData(Userid){
     if(Userid!==null &&Userid!==""){
--- src/app/shared/CustomerService.ts
+++ src/app/shared/CustomerService.ts
@@ -24,8 +24,21 @@ getAllCreditLimitforDashboard(UserCode): Observable<any> {
   return this.http.get(this.BaseURI + '/Creditlimit/GetCreditlimit/'+UserCode);
 } 
 
-
 getCustomerData(Id) {
   return this.http.get(this.BaseURI + '/CustomerMaster/GetCustomerDataByUserId/'+Id);
+}
+
+GetShipToAddress(Id) {
+  return this.http.get(this.BaseURI + '/CustomerMaster/GetShipToAddress/'+Id);
+}
+
+getGetShipToData(Id) {
+  return this.http.get(this.BaseURI + '/CustomerMaster/GetShipTo/'+Id);
+}
+
+
+
+GetTopItemMaster() {
+  return this.http.get(this.BaseURI + '/CustomerMaster/GetTopItemMaster');
 }
 }
